928911e00ca64fa22d87b90b05235587e948b234
Aravind S <aravindprime123@gmail.com>
2025-09-08 10:10:52 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h5 className="w-full text-3xl font-bold underline justify-center bg-amber-600 align-middle">
+          This is the the portfolio website for Nisha
+        </h5>
+      </div>
+    </>
+  );
+}
+
+export default App;
