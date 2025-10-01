060276536b641ad88408aafdd14751d3d2e4a283
Aravind S <aravindprime123@gmail.com>
2025-10-01 15:21:02 +0530

    Loader function completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(true);
+      return () => clearTimeout(timer);
+    }, 4000);
+  }, []);
+
   return (
     <>
-      <div className="h-screen w-screen bg-hero">
-        <h2 className=" text-3xl font-bold underline ">
-          This is the the portfolio website for Nisha
-        </h2>
-      </div>
+      {isLoading ? (
+        <div className="loading-container">
+          <div className="loader">
+            <span className="loading-span">
+              <h4>
+                <b>Loading...</b>
+              </h4>
+            </span>
+          </div>
+        </div>
+      ) : (
+        <div className="h-screen w-screen bg-hero">
+          <h2 className=" text-3xl font-bold underline ">
+            This is the the portfolio website for Nisha
+          </h2>
+        </div>
+      )}
     </>
   );
 }
