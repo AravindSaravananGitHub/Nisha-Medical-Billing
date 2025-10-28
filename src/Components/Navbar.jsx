934686cae134beb3b74026ba93cdc9267362d370
Aravind S <aravindprime123@gmail.com>
2025-10-28 10:03:11 +0530

    Nav starting

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import "../CSS/navbar.css";
 
 const Navbar = () => {
+  const navItems = ["About", "Experience", "Projects", "Contacts"];
+
   return (
     <div className="nav-container">
-      <div className="nav-logo">
-        <div className="logo">
-          <h3>
-            Nisha <span>Ravikumar</span>
-          </h3>
-        </div>
+      <div className="nav-content">
+        {navItems.map((item, itemPosition) => (
+          <ul key={itemPosition}>
+            <li>{item}</li>
+          </ul>
+        ))}
       </div>
-      <div className="nav-content"></div>
+
+      <div className="nav-logo">
+        <h3>
+          Nisha <span>Ravikumar</span>
+        </h3>
+      </div>
     </div>
   );
 };
