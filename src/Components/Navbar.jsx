ff072f3360e887923259fc5413e0d6da59730532
Aravind S <aravindprime123@gmail.com>
2025-10-29 10:04:49 +0530

    Nav structure

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "../CSS/navbar.css";
+import MarkunreadIcon from "@mui/icons-material/Markunread";
+import CallIcon from "@mui/icons-material/Call";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 const Navbar = () => {
   const navItems = ["About", "Experience", "Projects", "Contacts"];
@@ -7,17 +10,42 @@ const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="nav-content">
-        {navItems.map((item, itemPosition) => (
-          <ul key={itemPosition}>
-            <li>{item}</li>
-          </ul>
-        ))}
+        <ul className="nav-menu">
+          {navItems.map((item, itemPosition) => (
+            <li key={itemPosition}>
+              <a href="#">{item}</a>
+            </li>
+          ))}
+        </ul>
       </div>
 
       <div className="nav-logo">
         <h3>
-          Nisha <span>Ravikumar</span>
+          <a href="#">
+            <span>NISHA </span>
+            <span>RAVIKUMAR</span>
+          </a>
         </h3>
+      </div>
+
+      <div className="nav-contacts">
+        <ul>
+          <li>
+            <a href="#">
+              <MarkunreadIcon />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <CallIcon />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <WhatsAppIcon />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
