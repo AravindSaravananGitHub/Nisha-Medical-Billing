d915d7525c155416b1491e82cbde2f049da54c7d
Aravind S <aravindprime123@gmail.com>
2025-12-18 10:12:39 +0530

    Mobile Menu completed

--- src/Components/MobilMenu.jsx
+++ src/Components/MobilMenu.jsx
@@ -1,19 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../CSS/mobileMenu.css";
 import MenuIcon from "@mui/icons-material/Menu";
+import gsap from "gsap";
 
 const MobilMenu = () => {
   const [open, setOpen] = useState(false);
+  const containerRef = useRef(null);
 
-  const menus = ["Home", "About", "Experience", "Software", "Contact"];
+  const menus = ["Home", "About", "Experience", "Projects", "Contact"];
 
+  // Open Menu
   const openMenu = () => {
     setOpen(true);
+    gsap.to(containerRef.current, {
+      width: 256,
+      height: 256,
+      borderRadius: 20,
+      duration: 0.4,
+      ease: "back.out(1.6)",
+    });
   };
+
+  // Close Menu
+  const closeMenu = () => {
+    gsap.to(containerRef.current, {
+      width: 56,
+      height: 56,
+      borderRadius: 20,
+      duration: 0.3,
+      ease: "power2.in",
+      onComplete: () => setOpen(false),
+    });
+  };
+
+  // Outside click close menu
+  useEffect(() => {
+    const handleOutSideClick = (e) => {
+      if (
+        open &&
+        containerRef.current &&
+        !containerRef.current.contains(e.target)
+      ) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutSideClick);
+    return () => document.removeEventListener("mousedown", handleOutSideClick);
+  }, [open]);
 
   return (
     <div className="burger-wrap">
-      <div className="burger-container">
+      <div className="burger-container" ref={containerRef}>
         {!open && (
           <button className="burger-icon" onClick={openMenu}>
             <MenuIcon />
@@ -21,10 +59,12 @@ const MobilMenu = () => {
         )}
 
         {open && (
-          <ul>
+          <ul className="menu-list">
             {menus.map((item, index) => (
               <li key={index}>
-                <a href="#">{item}</a>
+                <a href={`#${item.toLowerCase}`} onClick={closeMenu}>
+                  {item}
+                </a>
               </li>
             ))}
           </ul>
